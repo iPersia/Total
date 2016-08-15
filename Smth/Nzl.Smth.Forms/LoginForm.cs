09edf58517a7d0cbc257df408c02bf0e34ff8c2a
Nesus <msnesus@hotmail.com>
2016-08-15 16:44:27 +0800

    Add new eventhandler.

--- Smth/Nzl.Smth.Forms/LoginForm.cs
+++ Smth/Nzl.Smth.Forms/LoginForm.cs
@@ -12,26 +12,58 @@
     using System.Windows.Forms;
     using Nzl.Web.Page;
     using Nzl.Web.Util;
-    using Nzl.Smth.Datas;
-    using Nzl.Smth.Utils;
+    using Nzl.Smth.Common;
 
     /// <summary>
     /// 
     /// </summary>
     public partial class LoginForm : BaseForm
     {
+        #region Singleton
         /// <summary>
         /// 
         /// </summary>
         public static readonly LoginForm Instance = new LoginForm();
+        #endregion
 
+        #region event
+        /// <summary>
+        /// 
+        /// </summary>
+        public event EventHandler<MessageEventArgs> OnLoginFailed;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public event EventHandler<MessageEventArgs> OnLogoutFailed;
+        #endregion
+
+        #region Ctor.
         /// <summary>
         /// 
         /// </summary>
         LoginForm()
         {
             InitializeComponent();
-            this.lcLog.OnLogCompleted += LcLog_LogCompleted;
+            this.lcLog.OnLoginCompleted += LcLog_OnLoginCompleted;
+            this.lcLog.OnLoginFailed += LcLog_OnLoginFailed;
+            this.lcLog.OnLogoutCompleted += LcLog_OnLogoutCompleted;
+            this.lcLog.OnLogoutFailed += LcLog_OnLogoutFailed;
+        }
+        #endregion
+
+        #region eventhandler
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void LcLog_OnLogoutFailed(object sender, MessageEventArgs e)
+        {
+            if (this.OnLogoutFailed!= null)
+            {
+                this.OnLogoutFailed(sender, e);
+            }
         }
 
         /// <summary>
@@ -39,9 +71,33 @@
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void LcLog_LogCompleted(object sender, EventArgs e)
+        private void LcLog_OnLogoutCompleted(object sender, EventArgs e)
         {
             this.Hide();
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void LcLog_OnLoginFailed(object sender, MessageEventArgs e)
+        {
+            if (this.OnLoginFailed != null)
+            {
+                this.OnLoginFailed(sender, e);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void LcLog_OnLoginCompleted(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+        #endregion
     }
 }
