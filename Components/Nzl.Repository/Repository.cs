3a7813700c8f31ba2bcc3fef8d95d683c7b9b673
Nesus <msnesus@hotmail.com>
2016-08-21 21:17:47 +0800

    Add methods.

--- Components/Nzl.Repository/Repository.cs
+++ Components/Nzl.Repository/Repository.cs
@@ -1,14 +1,48 @@
 ﻿namespace Nzl.Repository
 {
-    using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
 
     /// <summary>
     /// 
     /// </summary>
     public static class Repository
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        private static Dictionary<string, object> _staticDictionary = new Dictionary<string, object>();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static T GetValue<T>(string key)
+            where T : class
+        {
+            if (_staticDictionary.ContainsKey(key))
+            {
+                return _staticDictionary[key] as T;
+            }
+
+            return default(T);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static void Add<T>(string key, T value)
+            where T : class
+        {
+            lock(_staticDictionary)
+            {
+                if (_staticDictionary.ContainsKey(key) == false)
+                {
+                    _staticDictionary.Add(key, value);
+                }
+            }
+        }
     }
 }
